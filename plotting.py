b6ad5b84a18a016d8dba65766eb4ff4d9e34418c
zbuseaydin <88784379+zbuseaydin@users.noreply.github.com>
2022-11-26 22:07:05 +0300

    format

--- plotting.py
+++ plotting.py
@@ -2,26 +2,24 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 
-x_arr = []
+x_axis = []
 for i in range(100):
-    x_arr.append(1)
-    x_arr.append(2)
-    x_arr.append(5)
-    x_arr.append(10)
+    x_axis.append(1)
+    x_axis.append(2)
+    x_axis.append(5)
+    x_axis.append(10)
 
-x = np.array(x_arr)
-
+x = np.array(x_axis)
 
 os.system("make")
-
 my_list = ["1", "2", "5", "10"]
 y_axis = []
+
 for i in range(100):
     for no in my_list:
         os.system("./myprogram.o 10000 " + no)
-        my_file = open("output"+no+".txt", "r")
+        my_file = open("output" + no + ".txt", "r")
         y_axis.append(float(my_file.readlines()[-1]))
-
 y = np.array(y_axis)
 
 plt.scatter(x, y)
