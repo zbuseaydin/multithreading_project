0e9c680ca6ad96245bfd0fa02d87e05b5187dd60
zbuseaydin <zeynpaydinn@gmail.com>
2022-11-26 21:10:17 +0300

    name labels

--- plotting.py
+++ plotting.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x_arr = []
-for i in range(50):
+for i in range(100):
     x_arr.append(1)
     x_arr.append(2)
     x_arr.append(5)
@@ -16,13 +16,16 @@ os.system("make")
 
 my_list = ["1", "2", "5", "10"]
 y_axis = []
-for i in range(50):
+for i in range(100):
     for no in my_list:
-        os.system("./myprogram.o 1000 " + no)
+        os.system("./myprogram.o 10000 " + no)
         my_file = open("output"+no+".txt", "r")
         y_axis.append(float(my_file.readlines()[-1]))
 
 y = np.array(y_axis)
+
 plt.scatter(x, y)
-plt.title("Number of integers = 50")
+plt.xlabel("Number of threads")
+plt.ylabel("Execution time")
+plt.title("Array size = 10000")
 plt.show()
